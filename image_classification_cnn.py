b1f7941e422fd46927d7bf97aa4765e1b247d5b5
Andrea Vu <36121383+thuphuong1401@users.noreply.github.com>
2019-05-21 08:42:28 +0700

    Create image_classification_cnn.py

---
+++ image_classification_cnn.py
@@ -0,0 +1,84 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Import Tensorflow and Tensorflow datasets
+import tensorflow as tf
+import tensorflow_datasets as tfds
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+# Import libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Import progress bar display (not important though)
+import tqdm
+import tqdm.auto
+tqdm.tqdm = tqdm.auto.tqdm
+
+print(tf.__version__)
+
+# This will go away in the future.
+# If this gives an error, you might be running TensorFlow 2 or above
+# If so, the just comment out this line and run this cell again
+tf.enable_eager_execution()
+
+dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+# Class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal',      'Shirt',   'Sneaker',  'Bag',   'Ankle boot']
+
+# Show number of training and testing examples
+num_train_examples = metadata.splits['train'].num_examples
+num_test_examples = metadata.splits['test'].num_examples
+print("Number of training examples: {}".format(num_train_examples))
+print("Number of test examples:     {}".format(num_test_examples))
+
+# Normalize the pixel value within the range 0-1
+def normalize(images, labels):
+    images = tf.cast(images, tf.float32)
+    images /= 255
+    return images, labels
+
+# Normalize every image in the dataset
+train_dataset =  train_dataset.map(normalize)
+test_dataset  =  test_dataset.map(normalize)
+
+# Take a single image, remove the color dimension by reshaping
+for image, label in test_dataset.take(1):
+    break
+image = image.numpy().reshape((28,28))
+
+#Da model (booommmm) - Da CNN
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation=tf.nn.relu, input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D((2,2), strides = 2),
+    tf.keras.layers.Conv2D(64, (3,3), padding = 'same', activation=tf.nn.relu),
+    tf.keras.layers.MaxPooling2D((2,2), strides = 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# Versus no CNN - just a generic neural network
+#model = tf.keras.Sequential([
+#    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+#    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+#    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+#])
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+BATCH_SIZE = 32
+train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+# Perform training
+model.fit(train_dataset, epochs=10, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
+
+# Evaluate the accuracy of the model
+# The accuracy of this CNN model is after 10 epoches. Quite impressive then
+test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
+print('Accuracy on the dataset:', test_accuracy)
